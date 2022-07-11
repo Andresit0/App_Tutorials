682f829dd0dca91d9a29b7c958f341649cf81f2e
Andresit0 <rvandres001@gmail.com>
2022-07-11 11:56:02 -0500

    adding current developing projects

--- tutorials/lib/ui/andresRiofrio/andresRiofrioUI.dart
+++ tutorials/lib/ui/andresRiofrio/andresRiofrioUI.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:temp_btn_color_dialog_font/ui/chialispTutorial/chialisp.dart';
 import 'package:temp_btn_color_dialog_font/ui/gitTutorial/git.dart';
+import 'package:temp_btn_color_dialog_font/ui/global/slidable/clr.dart';
 import 'package:temp_btn_color_dialog_font/ui/postgresTutorial/postgresUI.dart';
 import 'dart:ui';
+import '../global/dialog.dart';
+import '../global/pageView/dotIndicator.dart';
 import '/ui/flutterTutorial/flutterUI.dart';
 import '../global/func.dart' as globalFunc;
 import '/ui/andresRiofrio/iconAppPublished.dart' as globalIcon;
@@ -22,6 +25,48 @@ class AndresRiofrio extends StatefulWidget {
 
 class AndresRiofrioPageState extends State<AndresRiofrio> {
   bool activateColor = false;
+  final List<Widget> pagesProFactec = <Widget>[
+    new ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: new Image.asset(globalVar.imgProFactecIntro),
+    ),
+    new ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: new Image.asset(globalVar.imgProFactec1_conf),
+    ),
+    new ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: new Image.asset(globalVar.imgProFactec2_detalleVentas),
+    ),
+    new ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: new Image.asset(globalVar.imgProFactec3_estadisticas),
+    ),
+    new ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: new Image.asset(globalVar.imgProFactec4_permisos),
+    ),
+    new ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: new Image.asset(globalVar.imgProFactec5_inventario),
+    ),
+    new ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: new Image.asset(globalVar.imgProFactec6_inventario),
+    ),
+    new ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: new Image.asset(globalVar.imgProFactec7_productos),
+    ),
+    new ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: new Image.asset(globalVar.imgProFactec8_sistemaVentas),
+    ),
+    new ConstrainedBox(
+      constraints: const BoxConstraints.expand(),
+      child: new Image.asset(globalVar.imgProFactec9_cuenta),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     double screenConstWidth = (MediaQuery.of(context).size.width -
@@ -207,6 +252,49 @@ class AndresRiofrioPageState extends State<AndresRiofrio> {
       );
     }
 
+    Widget pageView(List<Widget> pages) {
+      final _controller = new PageController();
+      const _kDuration = const Duration(milliseconds: 300);
+      const _kCurve = Curves.ease;
+
+      return IconTheme(
+        data: new IconThemeData(color: Colors.black),
+        child: new Stack(
+          children: <Widget>[
+            new PageView.builder(
+              physics: new AlwaysScrollableScrollPhysics(),
+              controller: _controller,
+              itemBuilder: (BuildContext context, int index) {
+                return pages[index % pages.length];
+              },
+            ),
+            new Positioned(
+              bottom: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: new Container(
+                color: Colors.grey[800],
+                padding: const EdgeInsets.all(20.0),
+                child: new Center(
+                  child: new DotsIndicator(
+                    controller: _controller,
+                    itemCount: pages.length,
+                    onPageSelected: (int page) {
+                      _controller.animateToPage(
+                        page,
+                        duration: _kDuration,
+                        curve: _kCurve,
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     Widget presentation() {
       if (screenConstHeight > screenConstWidth) return presentationRow();
       return Container(child: presentationColumn());
@@ -332,6 +420,163 @@ class AndresRiofrioPageState extends State<AndresRiofrio> {
                               ],
                             ),
                           ),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 10)),
+                        Container(
+                          width: double.infinity,
+                          child: globalFont.strDeveloperBody(
+                              'Current Developing Projects',
+                              false,
+                              Colors.black,
+                              size() * 5,
+                              TextAlign.left),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 10)),
+                        Row(
+                          children: [
+                            Container(
+                              width: size() * 29,
+                              height: size() * 29,
+                              child: globalBtn.btnImg(
+                                  Colors.transparent,
+                                  Colors.transparent,
+                                  size() * 90,
+                                  360,
+                                  0,
+                                  Colors.transparent,
+                                  globalVar.imgCocktail, () async {
+                                dialogHeaderBodyFooter(
+                                    context,
+                                    true,
+                                    primaryColor,
+                                    Text('COCKTAIL GAME',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        )),
+                                    Colors.white,
+                                    pageView(<Widget>[
+                                      ConstrainedBox(
+                                        constraints:
+                                            const BoxConstraints.expand(),
+                                        child: Column(
+                                          children: [
+                                            globalBtn.btnImg(
+                                                Colors.transparent,
+                                                Colors.transparent,
+                                                size() * 50,
+                                                360,
+                                                0,
+                                                Colors.transparent,
+                                                globalVar.imgWeb, () {
+                                              globalFunc.openAWeb(
+                                                  "http://97b4-190-110-60-140.ngrok.io/cocktail_game/#/");
+                                            }),
+                                            Padding(
+                                                padding: EdgeInsets.only(
+                                                    top: size() * 2))
+                                          ],
+                                        ),
+                                      ),
+                                      ConstrainedBox(
+                                        constraints:
+                                            const BoxConstraints.expand(),
+                                        child: Column(
+                                          children: [
+                                            globalBtn.btnImg(
+                                                Colors.transparent,
+                                                Colors.transparent,
+                                                size() * 48,
+                                                360,
+                                                0,
+                                                Colors.transparent,
+                                                globalVar.imgPlayStore, () {
+                                              globalFunc.openAWeb(
+                                                  "https://97b4-190-110-60-140.ngrok.io/cocktail_file/cocktail_apk.zip");
+                                            }),
+                                            Padding(
+                                                padding: EdgeInsets.only(
+                                                    top: size() * 2))
+                                          ],
+                                        ),
+                                      ),
+                                      ConstrainedBox(
+                                        constraints:
+                                            const BoxConstraints.expand(),
+                                        child: Column(
+                                          children: [
+                                            globalBtn.btnImg(
+                                                Colors.transparent,
+                                                Colors.transparent,
+                                                size() * 48,
+                                                360,
+                                                0,
+                                                Colors.transparent,
+                                                globalVar.imgWindows, () {
+                                              globalFunc.openAWeb(
+                                                  "https://97b4-190-110-60-140.ngrok.io/cocktail_file/cocktail_windows.zip");
+                                            }),
+                                            Padding(
+                                                padding: EdgeInsets.only(
+                                                    top: size() * 2))
+                                          ],
+                                        ),
+                                      ),
+                                      ConstrainedBox(
+                                        constraints:
+                                            const BoxConstraints.expand(),
+                                        child: Column(
+                                          children: [
+                                            globalBtn.btnImg(
+                                                Colors.transparent,
+                                                Colors.transparent,
+                                                size() * 48,
+                                                360,
+                                                0,
+                                                Colors.transparent,
+                                                globalVar.imgApple, () {
+                                              globalFunc.openAWeb(
+                                                  "https://97b4-190-110-60-140.ngrok.io/cocktail_file/cocktail_mac.zip");
+                                            }),
+                                            Padding(
+                                                padding: EdgeInsets.only(
+                                                    top: size() * 2))
+                                          ],
+                                        ),
+                                      ),
+                                    ]),
+                                    [],
+                                    size() * 60,
+                                    size() * 70);
+                              }),
+                            ),
+                            Padding(padding: EdgeInsets.only(left: 10)),
+                            Container(
+                              width: size() * 28,
+                              height: size() * 28,
+                              child: globalBtn.btnImg(
+                                  Colors.transparent,
+                                  Colors.transparent,
+                                  size() * 90,
+                                  360,
+                                  0,
+                                  Colors.transparent,
+                                  globalVar.imgProFactec, () {
+                                dialogHeaderBodyFooter(
+                                    context,
+                                    true,
+                                    primaryColor,
+                                    Text('PRO FACTEC',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        )),
+                                    Colors.white,
+                                    pageView(pagesProFactec),
+                                    [],
+                                    size() * 200,
+                                    size() * 228);
+                              }),
+                            ),
+                          ],
                         ),
                         Padding(padding: EdgeInsets.only(top: 10)),
                         Container(
--- tutorials/lib/ui/global/var.dart
+++ tutorials/lib/ui/global/var.dart
@@ -13,3 +13,27 @@ String imgFlutterColor = 'lib/ui/img/tutorial/flutter/colorExample.png';
 String imgFlutterForm = 'lib/ui/img/tutorial/flutter/formExample.png';
 String imgFlutterWriteRead = 'lib/ui/img/tutorial/flutter/writeReadExample.png';
 String imgFlutterLatex = 'lib/ui/img/tutorial/flutter/latexExample.jpeg';
+String imgWeb = 'lib/ui/img/icon/web.png';
+String imgPlayStore = 'lib/ui/img/icon/playStore.png';
+String imgWindows = 'lib/ui/img/icon/windows.png';
+String imgApple = 'lib/ui/img/icon/apple.png';
+String imgCocktail = 'lib/ui/img/appDeveloping/cocktail/cocktail.png';
+String imgProFactec = 'lib/ui/img/appDeveloping/proFactec/proFactec.png';
+String imgProFactecIntro =
+    'lib/ui/img/appDeveloping/proFactec/proFactecIntro.png';
+String imgProFactec1_conf = 'lib/ui/img/appDeveloping/proFactec/1_conf.png';
+String imgProFactec2_detalleVentas =
+    'lib/ui/img/appDeveloping/proFactec/2_detalleVentas.png';
+String imgProFactec3_estadisticas =
+    'lib/ui/img/appDeveloping/proFactec/3_estadisticas.png';
+String imgProFactec4_permisos =
+    'lib/ui/img/appDeveloping/proFactec/4_permisos.png';
+String imgProFactec5_inventario =
+    'lib/ui/img/appDeveloping/proFactec/5_inventario.png';
+String imgProFactec6_inventario =
+    'lib/ui/img/appDeveloping/proFactec/6_inventario.png';
+String imgProFactec7_productos =
+    'lib/ui/img/appDeveloping/proFactec/7_productos.png';
+String imgProFactec8_sistemaVentas =
+    'lib/ui/img/appDeveloping/proFactec/8_sistemaVentas.png';
+String imgProFactec9_cuenta = 'lib/ui/img/appDeveloping/proFactec/9_cuenta.png';
